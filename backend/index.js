cb446fe26f6d3e9aa04be22cd97c3c0eec24eb4c
harshbrahamwanshi <harshkumarbrahamwanshi@gmail.com>
2026-07-04 15:39:58 +0530

    Initial commit

---
+++ backend/index.js
@@ -0,0 +1,33 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./utils/db.js";
+dotenv.config({});
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+const corsOptions = {
+  origin: "http://localhost:5173",
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+
+// app.get("/home", (req, res) => {
+//   return res.status(200).json({
+//     message: "I am coming from backend",
+//     success: true,
+//   });
+// });
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    connectDB();
+  console.log(`Server Running at port ${PORT}`);
+});
