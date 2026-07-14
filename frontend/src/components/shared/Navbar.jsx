312de665fff465114bcf642f9150ef36213444f0
harshbrahamwanshi <harshkumarbrahamwanshi@gmail.com>
2026-07-14 16:27:47 +0530

    FrontEnd Setup

---
+++ frontend/src/components/shared/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div className="bg-white">
+      <div>
+        <h1 className="text-2xl font-bold">
+          Job<span className="text-[#F83002]">Portal</span>
+        </h1>
+      </div>
+      <div>
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
