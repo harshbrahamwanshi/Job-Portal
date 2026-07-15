6fc53a9006673ec14b88bbacd589cc520c640076
harshbrahamwanshi <harshkumarbrahamwanshi@gmail.com>
2026-07-15 13:25:34 +0530

    Login/Signup completed

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -1,11 +1,135 @@
-import React from 'react'
+import React, { useState } from "react";
+import Navbar from "../shared/Navbar";
+import { Label } from "../ui/label";
+import { Input } from "../ui/input";
+import { RadioGroup } from "../ui/radio-group";
+import { Button } from "../ui/button";
+import { Link, useNavigate } from "react-router-dom";
+import { toast } from "sonner";
+import axios from "axios";
+import { USER_API_END_POINT } from "@/utils/constant";
+import { useDispatch, useSelector } from "react-redux";
+import { setLoading } from "@/redux/authSlice";
+import { Loader, Loader2 } from "lucide-react";
 
 const login = () => {
-  return (
-    <div> 
-      Login
-    </div>
-  )
-}
+  const [input, setInput] = useState({
+    email: "",
+    password: "",
+    role: "",
+  });
+  
+  const {loading} = useSelector(store=>store.auth);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const changeEventHandler = (e) => {
+    setInput({ ...input, [e.target.name]: e.target.value });
+  };
 
-export default login
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    try {
+      dispatch(setLoading(true));
+      const res = await axios.post(`${USER_API_END_POINT}/login`, input, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        withCredentials: true,
+      });
+      if (res.data.success) {
+        navigate("/");
+        toast.success(res.data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.response?.data?.message || "Something went wrong");
+    }finally{
+      dispatch(setLoading(false));
+    }
+  };
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <Navbar />
+
+      <div className="flex items-center justify-center max-w-7xl mx-auto px-4">
+        <form
+          onSubmit={submitHandler}
+          className="w-full max-w-lg border border-gray-200 rounded-2xl p-8 my-10 shadow-lg bg-white"
+        >
+          <h1 className="font-bold text-3xl mb-6 text-center">Log In</h1>
+
+          <div className="mb-5 space-y-2">
+            <Label className="font-medium">Email</Label>
+            <Input
+              type="text"
+              value={input.email}
+              name="email"
+              onChange={changeEventHandler}
+              placeholder="Enter your email"
+            />{" "}
+          </div>
+
+          <div className="mb-5 space-y-2">
+            <Label className="font-medium">Password</Label>
+            <Input
+              type="password"
+              value={input.password}
+              name="password"
+              onChange={changeEventHandler}
+              placeholder="Enter your Password"
+            />
+          </div>
+
+          <div className="flex flex-col md:flex-row md:items-center md:justify-between gap-5">
+            <RadioGroup className="flex items-center gap-6">
+              <div className="flex items-center space-x-2">
+                <Input
+                  type="radio"
+                  name="role"
+                  value="student"
+                  checked={input.role == "student"}
+                  onChange={changeEventHandler}
+                  className="cursor-pointer"
+                />
+                <Label>Student</Label>
+              </div>
+
+              <div className="flex items-center space-x-2">
+                <Input
+                  type="radio"
+                  name="role"
+                  value="recruiter"
+                  checked={input.role == "recruiter"}
+                  onChange={changeEventHandler}
+                  className="cursor-pointer"
+                />
+                <Label>Recruiter</Label>
+              </div>
+            </RadioGroup>
+          </div>
+          {
+            loading?<Button className="w-full my-4"><Loader2 className="mr-2 h-4 w-4 animate-spin"/>Please wait</Button> : <Button
+            type="submit"
+            className="w-full bg-black text-white hover:bg-black/90 mt-6 py-5 text-base font-semibold"
+          >
+            Login
+          </Button>
+          }
+         
+
+          <span className="text-sm block text-center mt-4 text-gray-600">
+            Don't have an account?{" "}
+            <Link
+              to="/Signup"
+              className="text-blue-600 font-medium hover:underline"
+            >
+              Signup
+            </Link>
+          </span>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default login;
--- frontend/src/components/auth/Signup.jsx
+++ frontend/src/components/auth/Signup.jsx
@@ -1,74 +1,185 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../shared/Navbar";
 import { Label } from "../ui/label";
 import { Input } from "../ui/input";
-import { RadioGroup, RadioGroupItem } from "../ui/radio-group";
+import { RadioGroup } from "../ui/radio-group";
 import { Button } from "../ui/button";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { USER_API_END_POINT } from "@/utils/constant.js";
+import { toast } from "sonner";
+import { useDispatch, useSelector } from "react-redux";
+import { setLoading } from "@/redux/authSlice";
+import { Loader2 } from "lucide-react";
 
 const signup = () => {
+  const [input, setInput] = useState({
+    fullname: "",
+    email: "",
+    phoneNumber: "",
+    password: "",
+    role: "",
+    file: "",
+  });
+
+   const {loading} = useSelector(store=>store.auth);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const changeEventHandler = (e) => {
+    setInput({ ...input, [e.target.name]: e.target.value });
+  };
+
+  const changeFileHandler = (e) => {
+    setInput({ ...input, file: e.target.files?.[0] });
+  };
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("fullname", input.fullname);
+    formData.append("email", input.email);
+    formData.append("phoneNumber", input.phoneNumber);
+    formData.append("password", input.password);
+    formData.append("role", input.role);
+    if (input.file) {
+      formData.append("file", input.file);
+    }
+    try {
+      dispatch(setLoading(true));
+      const res = await axios.post(`${USER_API_END_POINT}/register`, formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+        withCredentials: true,
+      });
+      if (res.data.success) {
+        navigate("/login");
+        toast.success(res.data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.response.data.message);
+    }finally{
+          dispatch(setLoading(false));
+        }
+  };
   return (
-    <div>
+    <div className="min-h-screen bg-gray-50">
       <Navbar />
-      <div className="flex items-center justify-center max-w-7xl mx-auto">
+<Button
+  type="button"
+  onClick={() => toast.success("Test toast")}
+>
+  Test Toast
+</Button>
+      <div className="flex items-center justify-center max-w-7xl mx-auto px-4">
         <form
-          action=""
-          className="w-1/2 border border-gray-200 rounded-md p-4 my-10"
+          onSubmit={submitHandler}
+          className="w-full max-w-lg border border-gray-200 rounded-2xl p-8 my-10 shadow-lg bg-white"
         >
-          <h1 className="font-bold text-xl mb-5">Sign Up</h1>
-          <div className="my-2">
-            <Label>Full Name</Label>
-            <Input type="text" placeholder="Enter your name" />
+          <h1 className="font-bold text-3xl mb-6 text-center">Sign Up</h1>
+
+          <div className="mb-5 space-y-2">
+            <Label className="font-medium">Full Name</Label>
+            <Input
+              type="text"
+              value={input.fullname}
+              name="fullname"
+              onChange={changeEventHandler}
+              placeholder="Enter your name"
+            />
           </div>
-          <div className="my-2">
-            <Label>Email</Label>
-            <Input type="text" placeholder="Enter your email" />
+
+          <div className="mb-5 space-y-2">
+            <Label className="font-medium">Email</Label>
+            <Input
+              type="text"
+              value={input.email}
+              name="email"
+              onChange={changeEventHandler}
+              placeholder="Enter your email"
+            />
           </div>
-          <div className="my-2">
-            <Label>Phone Number</Label>
-            <Input type="text" placeholder="Enter your Phone Number" />
+
+          <div className="mb-5 space-y-2">
+            <Label className="font-medium">Phone Number</Label>
+            <Input
+              type="text"
+              value={input.phoneNumber}
+              name="phoneNumber"
+              onChange={changeEventHandler}
+              placeholder="Enter your Phone Number"
+            />
           </div>
-          <div className="my-2">
-            <Label>Password</Label>
-            <Input type="text" placeholder="Enter your Password" />
+
+          <div className="mb-5 space-y-2">
+            <Label className="font-medium">Password</Label>
+            <Input
+              type="password"
+              value={input.password}
+              name="password"
+              onChange={changeEventHandler}
+              placeholder="Enter your Password"
+            />
           </div>
-          <div className="flex items-center justify-between">
-            <RadioGroup className="flex items-center gap-4 my-5">
+
+          <div className="flex flex-col md:flex-row md:items-center md:justify-between gap-5">
+            <RadioGroup className="flex items-center gap-6">
               <div className="flex items-center space-x-2">
                 <Input
-                type="radio"
-                name="role"
-                value="student"
-                className="cursor-pointer"
+                  type="radio"
+                  name="role"
+                  value="student"
+                  checked={input.role == "student"}
+                  onChange={changeEventHandler}
+                  className="cursor-pointer"
                 />
-                <Label htmlFor="r1">Student</Label>
+                <Label>Student</Label>
               </div>
+
               <div className="flex items-center space-x-2">
                 <Input
-                type="radio"
-                name="role"
-                value="Recruiter"
-                className="cursor-pointer"
+                  type="radio"
+                  name="role"
+                  value="recruiter"
+                  checked={input.role == "recruiter"}
+                  onChange={changeEventHandler}
+                  className="cursor-pointer"
                 />
-                <Label htmlFor="r2">Recuiter</Label>
+                <Label>Recruiter</Label>
               </div>
             </RadioGroup>
-            <div className="flex items-center gap-2">
-              <Label>Profile</Label>
+
+            <div className="flex items-center gap-3">
+              <Label className="font-medium">Profile</Label>
               <Input
-              accept="image/*"
-              type="file"
-              className="cursor-pointer"
+                accept="image/*"
+                type="file"
+                onChange={changeFileHandler}
+                className="cursor-pointer max-w-[200px]"
               />
             </div>
           </div>
-          <Button
-  type="submit"
-  className="w-full bg-black text-white hover:bg-black/90"
->
-  SignUp
-</Button>
-<span className="text-sm">Already have an account? <Link to="/login" className="text-blue-500">Login</Link></span>
+          {
+            loading?<Button className="w-full my-4"><Loader2 className="mr-2 h-4 w-4 animate-spin"/>Please wait</Button> :          <Button
+            type="submit"
+            className="w-full bg-black text-white hover:bg-black/90 mt-6 py-5 text-base font-semibold"
+          >
+            Sign Up
+          </Button>
+          }
+
+
+          <span className="text-sm block text-center mt-4 text-gray-600">
+            Already have an account?{" "}
+            <Link
+              to="/login"
+              className="text-blue-600 font-medium hover:underline"
+            >
+              Login
+            </Link>
+          </span>
         </form>
       </div>
     </div>
