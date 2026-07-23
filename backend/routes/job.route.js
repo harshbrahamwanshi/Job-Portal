d1196eaa488ec398cf72fa2f78a0c4b1727b19c8
harshbrahamwanshi <harshkumarbrahamwanshi@gmail.com>
2026-07-23 11:42:49 +0530

    till 11:05:57

--- backend/routes/job.route.js
+++ backend/routes/job.route.js
@@ -4,11 +4,9 @@ import { getAdminJobs, getAllJobs, getJobById, postJob } from "../controllers/jo
 
 const router = express.Router();
 
-router.route("/post").post(isAuthenticated,postJob);
-router.route("/get").get(isAuthenticated,getAllJobs);
-router.route("/getadminjobs").post(isAuthenticated,getAdminJobs);
-router.route("/get/:id").get(isAuthenticated,getJobById);
+router.route("/post").post(isAuthenticated, postJob);
+router.route("/get").get(isAuthenticated, getAllJobs);
+router.route("/getadminjobs").get(isAuthenticated, getAdminJobs);
+router.route("/get/:id").get(isAuthenticated, getJobById);
 
 export default router;
-
-
